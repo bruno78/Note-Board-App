52c3dff4930339f209146f1dceae8eeaa1991b00
bruno78 <devbruno78@gmail.com>
2017-07-11 00:33:30 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Note Board App
+
+A react application to pin your notes on the board like Post-its.
+This is the full version application, organized into components.
+
+To see the live example go [here]("https://codepen.io/bruno78/full/weQRYj/")
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Board from './Board';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Board count={10} />, document.getElementById('react-container'));
+registerServiceWorker();
